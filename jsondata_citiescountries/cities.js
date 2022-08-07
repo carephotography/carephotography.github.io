a5458eee1383506a51e29d53bdff253a8424fafc
souocare <gonga1999@outlook.com>
2022-08-07 18:22:52 +0100

    Your Message

---
+++ jsondata_citiescountries/cities.js
@@ -0,0 +1,44 @@
+CITIES =
+[
+   {namecity: 'Lisbon', namecountry: 'Portugal', latitude: 38.706913, longitude: -9.136428, radius: 1, fillKey: 'city', date: ''},
+   // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
+   {namecity: 'Porto', namecountry: 'Portugal', latitude: 41.142661, longitude: -8.608225, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Braga', namecountry: 'Portugal', latitude: 41.551416, longitude: -8.423012, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Aveiro', namecountry: 'Portugal', latitude: 40.641190, longitude: -8.653620, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Coimbra', namecountry: 'Portugal', latitude: 40.207674277767566, longitude: -8.428634506854365, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Madeira Island, Madeira', namecountry: 'Portugal', latitude: 32.646884542946765, longitude: -16.913341530527358, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Porto Santo, Madeira', namecountry: 'Portugal', latitude: 33.06029031670386, longitude: -16.330899746017604, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'São Miguel, Azores', namecountry: 'Portugal', latitude: 37.78160403116633, longitude: -25.4910785308494, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Terceira, Azores', namecountry: 'Portugal', latitude: 38.720995140766824, longitude: -27.219447002724202, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Pico, Azores', namecountry: 'Portugal', latitude: 38.4809750428289, longitude: -28.401299526879438, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Madrid', namecountry: 'Spain', latitude: 40.411662604678796, longitude: -3.707637959284478, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Barcelona', namecountry: 'Spain', latitude: 41.40343269476518, longitude: 2.173607141691675, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Ayamonte', namecountry: 'Spain', latitude: 37.21421684160017, longitude: -7.40739195818186, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Paris', namecountry: 'France', latitude: 48.85833970961588, longitude: 2.29582051816227, radius: 1, fillKey: 'city', date: ''},
+   
+   {namecity: 'Edinburgh', namecountry: 'Scotland', latitude: 55.94797722557135, longitude: -3.201327198927578, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Manchester', namecountry: 'England', latitude: 53.47856082982635, longitude: -2.2449601298491446, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Blackpool', namecountry: 'England', latitude: 53.813635453726874, longitude: -3.056283355171498, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'London', namecountry: 'England', latitude: 51.50775922142887, longitude: -0.11870147379349309, radius: 1, fillKey: 'city', date: ''},
+
+   {namecity: 'Oslo', namecountry: 'Norway', latitude: 59.91747842225689, longitude: 10.728227717819097, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Copenhagen', namecountry: 'Denmark', latitude: 55.677127654312095, longitude: 12.569735751310281, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Gothenburg', namecountry: 'Sweden', latitude: 57.70898449677918, longitude: 11.973064935069024, radius: 1, fillKey: 'city', date: ''},
+
+   {namecity: 'Prague', namecountry: 'Czech Republic', latitude: 50.076665280502596, longitude: 14.430771027686282, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Budapest', namecountry: 'Hungary', latitude: 47.5072994655785, longitude: 19.046483710178194, radius: 1, fillKey: 'city', date: ''},
+
+   {namecity: 'Marrakech', namecountry: 'Morocco', latitude: 31.625854859466035, longitude: -7.988169816592562, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'İstanbul', namecountry: 'Turkey', latitude: 41.00966118033763, longitude: 28.975179758383863, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Amsterdam', namecountry: 'The Netherlands', latitude: 52.36380834082365, longitude: 4.886786744386697, radius: 1, fillKey: 'city', date: ''},
+
+   {namecity: 'Mostar', namecountry: 'Bosnia and Herzegovina', latitude: 43.34355940219241, longitude: 17.80832432593862, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Sarajevo', namecountry: 'Bosnia and Herzegovina', latitude: 43.85589959717472, longitude: 18.411173667144432, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Pocitelj', namecountry: 'Bosnia and Herzegovina', latitude: 43.139176185614794, longitude: 17.730489166453136, radius: 1, fillKey: 'city', date: ''},
+
+   {namecity: 'Bamberg', namecountry: 'Germany', latitude: 49.89734057020301, longitude: 10.895319174659106, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Munich', namecountry: 'Germany', latitude: 48.140165165357345, longitude: 11.589206134869563, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Nürnberg', namecountry: 'Germany', latitude: 49.44992814583952, longitude: 11.072978636993161, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Rothenburg ob der Tauber', namecountry: 'Germany', latitude: 49.37920623738108, longitude: 10.182419810714899, radius: 1, fillKey: 'city', date: ''},
+   {namecity: 'Würzburg', namecountry: 'Germany ', latitude: 49.78470256121875, longitude: 9.929293697602192, radius: 1, fillKey: 'city', date: ''},
+]
---
+++ jsondata_citiescountries/countries.js
@@ -0,0 +1,25 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+COUNTRIES =
+{
+    PRT: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    BEL: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    CHE: {fillKey: 'visited'},
+    ITA: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    CZE: {fillKey: 'visited'},
+    AUT: {fillKey: 'visited'},
+    HRV: {fillKey: 'visited'},
+    BIH: {fillKey: 'visited'},
+    GRC: {fillKey: 'visited'},
+    HUN: {fillKey: 'visited'},
+    SWE: {fillKey: 'visited'},
+    DNK: {fillKey: 'visited'},
+    NOR: {fillKey: 'visited'},
+    MAR: {fillKey: 'visited'},
+    TUR: {fillKey: 'visited'},
+    DOM: {fillKey: 'visited'},
+}
